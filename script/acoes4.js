64709c2cb78103c534b5a3952b696deb01793849
Gabriel <gabrielpedrosa346@gmail.com>
2025-01-23 07:57:16 -0300

    testes

---
+++ script/acoes4.js
@@ -0,0 +1,47 @@
+let c1 = document.getElementById('c1')
+let text1 = c1.querySelector('p')
+    c1.addEventListener('mouseenter', () => {
+        text1.style.display = 'block'
+    })
+    c1.addEventListener('mouseout', () => {
+        text1.style.display = 'none'
+    })
+    text1.addEventListener('mouseenter', () => {
+        text1.style.display = 'block'
+    })
+
+let c2 = document.getElementById('c2')
+let text2 = c2.querySelector('p')
+    c2.addEventListener('mouseenter', () => {
+        text2.style.display ='block'
+    })
+    c2.addEventListener('mouseout', () => {
+        text2.style.display = 'none'
+    })
+    text2.addEventListener('mouseenter', () => {
+        text2.style.display = 'block'
+    })
+
+let c3 = document.getElementById('c3')
+let text3 = c3.querySelector('p')
+    c3.addEventListener('mouseenter', () => {
+        text3.style.display = 'block'
+    })
+    c3.addEventListener('mouseout', () => {
+        text3.style.display = 'none'
+    })
+    text3.addEventListener('mouseenter', () => {
+        text3.style.display = 'block'
+    })
+
+let c4 = document.getElementById('c4')
+let text4 = c4.querySelector('p')
+    c4.addEventListener('mouseenter', () => {
+        text4.style.display = 'block'
+    })
+    c4.addEventListener('mouseout', () => {
+        text4.style.display = 'none'
+    })
+    text4.addEventListener('mouseenter', () => {
+        text4.style.display = 'block'
+    })
