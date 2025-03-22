94fa6b252a37889a6a83bb2a6a12afa49966c892
Gabriel <gabrielpedrosa346@gmail.com>
2025-03-22 13:03:31 -0300

    Mudanças nos arquivos

---
+++ script/scrpt6.js
@@ -0,0 +1,18 @@
+/*let a = 1
+while (a <= 6) {
+    console.log(`Passo ${a}`)
+    a++
+}/* 
+      A while é chamada de estrutura de repetição com teste lógico no inicio, ela testa a condição primeiro e executa depois, se
+    a condição for verdadeira ela iniciarar o looping até que essa condição seja falsa.
+      Ex: uma pizza que tem 8 pedaços o while executarar a condição comerFatia() até que todas as 8 fatias da pizza tem sido
+    consumidas(um belo exemplo de repetição. kkkkkk)
+*/
+
+//-------------- ----------------------
+
+console.log('Vai começar...')
+for (let b = 1; b <= 5 ;b++) {
+    console.log(b)
+}
+console.log('Fim!')
